2352608951226e5cf3d82ff68073018fd6785ec6
Karol Grzesiak <karol.grzesiakk@gmail.com>
2020-04-11 16:27:06 +0200

    Partially completes Banking Microservice

--- MicroRabbit.Banking.API/Startup.cs
+++ MicroRabbit.Banking.API/Startup.cs
@@ -2,20 +2,50 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Infra.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using Microsoft.OpenApi.Models;
 namespace MicroRabbit.Banking.API
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddDbContext<BankingDbContext>(options =>
+                {
+                    options.UseSqlServer(Configuration.GetConnectionString("BankingDbConnection"), o=>o.MigrationsAssembly("MicroRabbit.Banking.Data"));
+                });
+            services.AddSwaggerGen(o =>
+            {
+                o.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Banking Microservice",
+                    Version = "V1"
+                });
+            });
+            services.AddMediatR(typeof(Startup));
+            RegisterServices(services);
+        }
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,13 +55,16 @@ namespace MicroRabbit.Banking.API
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(o => o.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking Microservice V1"));
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+                endpoints.MapDefaultControllerRoute();
+                endpoints.MapControllers();
             });
+            
         }
     }
 }
